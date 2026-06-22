e3751a6dcb1886fe2c3e5c06fb9b0a3b890e5155
MILENA WINER <49257336@ort>
2026-06-22 10:10:46 -0300

    se agregó la configuración de la base de datos

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express  from "express";
 import cors     from "cors";
-import ProvinceRouter from "./src/controllers/province-controller.js"
+import ProvinceController from "./src/controllers/province-controller.js"
 
 const app  = express();
-const port = 3000;       // El puerto 3000 (http://localhost:3000)
+const port = 3000;
 // Agrego los Middlewares
 app.use(cors());         // Middleware de CORS.
 app.use(express.json()); // Middleware para parsear y comprender JSON.
 //
 // Endpoints (todos los Routers)
 //
-app.use("/api/province", ProvinceRouter);
+app.use("/api/province", ProvinceController);
 //
 // Inicio el Server y lo pongo a escuchar.
 //
